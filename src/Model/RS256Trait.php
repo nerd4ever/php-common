895d238898c5a26c091f9c151e8fcbf60b889733
Sileno Brito <silenobrito@gmail.com>
2023-03-13 15:29:36 -0300

    added trait rs256

---
+++ src/Model/RS256Trait.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Nerd4ever\Common\Model;
+
+use Exception;
+
+trait RS256Trait
+{
+    /**
+     * Encode given data using a given key.
+     *
+     * @param mixed $privateKey
+     * @param string $data
+     * @return string|null
+     * @throws Exception
+     */
+    public function toRS256($privateKey, string $data): ?string
+    {
+        if (!is_string($privateKey) && !is_resource($privateKey)) throw new Exception('key is required');
+        $signature = null;
+        openssl_sign($data, $signature, $privateKey, 'SHA256');
+        return $signature;
+    }
+
+    /**
+     * Verica a assinatura usando a chave pública
+     *
+     * @param mixed $publicKey
+     * @param string $data
+     * @param string $signature
+     * @return bool
+     * @throws Exception
+     */
+    protected function testRS256($publicKey, string $data, string $signature): bool
+    {
+        if (!is_string($publicKey) && !is_resource($publicKey)) throw new Exception('key is required');
+        if (file_exists($publicKey)) $publicKey = file_get_contents($publicKey);
+        return openssl_verify($data, $signature, $publicKey, 'SHA256') !== false;
+    }
+}

ce1b5bb19c5a126e8e79f9b281058b9446f33137
Sileno Brito <silenobrito@gmail.com>
2023-05-11 14:13:43 -0300

    alterado a lib e adicionado truncate de 32

--- src/Tools/IdTools.php
+++ src/Tools/IdTools.php
@@ -2,28 +2,24 @@
 
 namespace Nerd4ever\Common\Tools;
 
+use Ramsey\Uuid\Uuid;
+
 class IdTools
 {
     public static function gen(): string
     {
-        return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-            // 32 bits for "time_low"
-            mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+        // Gerar um UUIDv4
+        $uuid = Uuid::uuid4();
 
-            // 16 bits for "time_mid"
-            mt_rand(0, 0xffff),
+        // Obter o UUID formatado
+        return $uuid->toString();
 
-            // 16 bits for "time_hi_and_version",
-            // four most significant bits holds version number 4
-            mt_rand(0, 0x0fff) | 0x4000,
 
-            // 16 bits, 8 bits for "clk_seq_hi_res",
-            // 8 bits for "clk_seq_low",
-            // two most significant bits holds zero and one for variant DCE1.1
-            mt_rand(0, 0x3fff) | 0x8000,
-
-            // 48 bits for "node"
-            mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
-        );
     }
+
+    public static function gen32(): string
+    {
+        return substr(IdTools::gen(), 0, 32);
+    }
+
 }

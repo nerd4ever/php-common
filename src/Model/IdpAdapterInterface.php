3c4405728db5cd663869415e0fb0067716087390
Sileno Brito <silenobrito@gmail.com>
2024-05-30 11:50:12 -0300

    update idp

--- src/Model/IdpAdapterInterface.php
+++ src/Model/IdpAdapterInterface.php
@@ -12,21 +12,5 @@ interface IdpAdapterInterface
 
     public function findUser(string $email, ?TenantInterface $tenant): ?FederationUser;
 
-    public function updateUser(FederationUser $user, TenantInterface $tenant): void;
-
-    public function createUser(FederationUser $user, TenantInterface $tenant): void;
-
-    public function removeUser(string $username): void;
-
-    public function revokeUser(string $username, TenantInterface $tenant): void;
-
-    public function addUserInGroup(FederationUser $sa, $groupName): void;
-
-    public function removeUserFromGroup(FederationUser $sa, $groupName): void;
-
     public function getAccessToken(): ?string;
-
-    public function getAccessTokenWith(string $username, string $password): ?string;
-
-    public function trustInUser(FederationUser $user, TenantInterface $tenant): void;
 }

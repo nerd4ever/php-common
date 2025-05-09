93544d94f998cdc7157399fb474b16ae194221e2
Sileno Brito <silenobrito@gmail.com>
2025-05-09 08:49:25 -0300

    added backup interface

---
+++ src/Model/BackupInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by Sileno de Oliveira Brito on 09/05/2025.
+ * Copyright (c) 2025 Nerd4ever All rights reserved.
+ */
+
+namespace Nerd4ever\Common\Model;
+
+interface BackupInterface
+{
+    public function exportData(): array;
+
+    public function importData(array $data): void;
+}
